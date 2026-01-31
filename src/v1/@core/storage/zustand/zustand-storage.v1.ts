ea7cc9ed0c57f1c70c4254a271bb274caf27499c
m4rc310 <mlsilva@coamo.com.br>
2026-01-31 17:14:00 -0300

    ....

---
+++ src/v1/@core/storage/zustand/zustand-storage.v1.ts
@@ -0,0 +1,33 @@
+import { create } from "zustand";
+import { persist } from "zustand/middleware";
+// import { v4 as uuidv4 } from "uuid";
+
+interface KanbanStore {
+	// mode: TOperationMode;
+  // columns: TKanbanServerStore["columns"];
+  // clientId?: string;
+  // hydrate: (data: TKanbanServerStore) => void;
+  // applyEvent: (event: TMainEvent) => void;
+}
+
+// const sortByOrder = <T extends { order: number }>(list: T[]) =>
+//   [...list].sort((a, b) => a.order - b.order);
+
+export const useKanbanStore = create<KanbanStore>()(
+  persist(
+    (set, get) => ({
+      // mode: "NORMAL",
+			// applyEvent(event) {
+			// 	switch (event.type) {
+			// 		case "CHANGE_OPERATION_MODE": {
+			// 			set({ mode: event.mode });
+			// 			break;
+			// 		}
+			// 	}
+			// },
+    }),
+    {
+      name: "kanban-storage",
+    },
+  ),
+);
